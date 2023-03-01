cc5367ecc4d31e7449e0ff9d4cf40d9eea158858
sickkeint223 <125326716+sickkeint223@users.noreply.github.com>
2023-03-02 10:11:13 +1300

    cube gravity
    did some cool shit

---
+++ toof/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    
+    
+    // Start is called before the first frame update
+    void Start ()
+    {
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
